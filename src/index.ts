79afbfc775c93fbcdadc82cbfb22ad893a67e4c8
VijayaKumarGavara <ggana6947@gmail.com>
2026-05-01 06:57:09 +0530

    Observe the type inference and it's issues

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-const msg:string="Hello World";
-console.log(msg);
+let username = "Vijay";
+let age = 22;
+let isStudent = true;
+
+const country="India"
+let c2="India"
+console.log(username);
+console.log(age);
+console.log(isStudent);
